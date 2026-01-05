d7bb4af9d6c711c68b5df8d31ea69b3c24eeead1
Andrea <andrea.cumbal@epn.edu.ec>
2026-01-05 18:30:15 -0500

    Manejo de los mapas de karnaugh con filas y columnas

--- src/main/java/Modelo/MapaKarnaugh.java
+++ src/main/java/Modelo/MapaKarnaugh.java
@@ -1,5 +1,42 @@
 package Modelo;
 
 public class MapaKarnaugh {
-    
+    private final int numeroVariables;
+
+    public MapaKarnaugh(int numeroVariables) {
+        this.numeroVariables = numeroVariables;
+    }
+
+    public int[][] construirMapa(boolean[] salidas) {
+
+        int filas = 1 << (numeroVariables / 2);
+        int columnas = 1 << (numeroVariables - numeroVariables / 2);
+
+        int[][] mapa = new int[filas][columnas];
+
+        int[] grayFilas = generarGray(filas);
+        int[] grayColumnas = generarGray(columnas);
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+
+                int indiceTabla =
+                        (grayFilas[i] << (numeroVariables - numeroVariables / 2))
+                                | grayColumnas[j];
+
+                mapa[i][j] = salidas[indiceTabla] ? 1 : 0;
+            }
+        }
+
+        return mapa;
+    }
+
+    private int[] generarGray(int n) {
+        int[] gray = new int[n];
+        for (int i = 0; i < n; i++) {
+            gray[i] = i ^ (i >> 1);
+        }
+        return gray;
+    }
 }
+
