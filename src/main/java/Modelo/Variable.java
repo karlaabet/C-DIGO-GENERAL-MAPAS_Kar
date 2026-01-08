50c73a86a30594f15840a37f30f9993caee815ad
matimongee1-png <mati.mongee1@gmail.com>
2026-01-07 20:28:14 -0500

    Se modifica el paquete ´Modelo´ y se agregan las clases respectivas a este

--- src/main/java/Modelo/Variable.java
+++ src/main/java/Modelo/Variable.java
@@ -1,5 +1,43 @@
 package Modelo;
 
 public class Variable {
-    
+
+    private final char nombre;
+    private final boolean negada;
+
+    public Variable(char nombre, boolean negada) {
+        this.nombre = nombre;
+        this.negada = negada;
+    }
+
+    public char getNombre() {
+        return nombre;
+    }
+
+    public boolean isNegada() {
+        return negada;
+    }
+
+    public Variable negar() {
+        return new Variable(nombre, !negada);
+    }
+
+    @Override
+    public String toString() {
+        return negada ? "¬" + nombre : String.valueOf(nombre);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Variable)) return false;
+
+        Variable otra = (Variable) obj;
+        return nombre == otra.nombre && negada == otra.negada;
+    }
+
+    @Override
+    public int hashCode() {
+        return (nombre * 31) + (negada ? 1 : 0);
+    }
 }
