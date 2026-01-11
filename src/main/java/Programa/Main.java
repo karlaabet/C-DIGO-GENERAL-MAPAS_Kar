64de540929f84505d4df6fd77ffc767363711496
Alexd <Alexd@Alex>
2026-01-11 16:06:41 -0500

    InterfazGrafica

--- src/main/java/Programa/Main.java
+++ src/main/java/Programa/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     
-   public static void main(String[] args) {
+ 
+     public static void main(String[] args) {
         new MenuConfiguracion().setVisible(true);
    }
 
-        
 }
