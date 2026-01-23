88c26e7b0db082d3be07341e6b5d96d8b9f6e6c8
SouthSkynet <alexismendieta810@gmail.com>
2026-01-23 07:37:07 -0500

    Implement equals, hashCode, and new methods in Term
    Added equals and hashCode methods for proper object comparison. Enhanced puedeCombinar and added cubreMintermino methods for additional functionality.

--- src/main/java/Lógica/Term.java
+++ src/main/java/Lógica/Term.java
@@ -1,5 +1,6 @@
-
 package Lógica;
+
+import java.util.Objects;
 
 public class Term {
     public String bits;
@@ -8,20 +9,44 @@ public class Term {
         this.bits = bits;
     }
 
-    public int diferencias(Term otro) {
+    // VITAL: Permite que List.contains() y HashSet funcionen correctamente
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Term term = (Term);
+        return Objects.equals(bits, term.bits);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bits);
+    }
+
+    // Asegura que los guiones estén en la misma posición antes de combinar
+    public boolean puedeCombinar(Term otro) {
         int dif = 0;
-        for (int i = 0; i < bits.length(); i++)
-            if (bits.charAt(i) != otro.bits.charAt(i)) dif++;
-        return dif;
+        for (int i = 0; i < bits.length(); i++) {
+            if ((this.bits.charAt(i) == '-') != (otro.bits.charAt(i) == '-')) return false;
+            if (this.bits.charAt(i) != otro.bits.charAt(i)) dif++;
+        }
+        return dif == 1;
     }
 
     public Term combinar(Term otro) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < bits.length(); i++) {
-            sb.append(bits.charAt(i) == otro.bits.charAt(i)
-                    ? bits.charAt(i)
-                    : '-');
+            sb.append(bits.charAt(i) == otro.bits.charAt(i) ? bits.charAt(i) : '-');
         }
         return new Term(sb.toString());
     }
+
+    public boolean cubreMintermino(int m, int vars) {
+        String bin = String.format("%" + vars + "s", Integer.toBinaryString(m)).replace(' ', '0');
+        for (int i = 0; i < bits.length(); i++) {
+            if (bits.charAt(i) != '-' && bits.charAt(i) != bin.charAt(i)) return false;
+        }
+        return true;
+    }
+    
 }
